b149ba373c9cc9018560779b44168a160e99f0e6
raspreet-github <168179350+raspreet-github@users.noreply.github.com>
2024-04-26 15:28:28 +0530

    Create myFirstActionScript.sh

---
+++ .github/workflows/scripts/myFirstActionScript.sh
@@ -0,0 +1,7 @@
+          echo "Hello World!"
+          echo "What's the Hostname of the server ?"
+          hostname -f
+          echo "What's the present working directory ?"
+          pwd
+          echo "What's the version of Java on the server ?"
+          java -version
